7d3f8a3732300741d2ff55629cc9b843e54ae83d
Manuel Morandin <manuel.morandin@gmail.com>
2026-01-04 20:42:35 +1100

    Added ValidationPipe to fix types validation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,18 @@ async function bootstrap() {
     //    ],
     //    credentials: true,
     // });
+
+  // Enable global validation
+  app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+  );
+
+
+
 
   // Swagger configuration
   const config = new DocumentBuilder()
